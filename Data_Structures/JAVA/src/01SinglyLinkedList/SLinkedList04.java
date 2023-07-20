a927fc6b7ef6ac51fddb29009aed223461eae180
Sufaina-Nesrin <sufainanasrin2003@gmail.com>
2023-07-20 18:06:34 +0530

    commited some changes

--- Data_Structures/JAVA/src/01SinglyLinkedList/SLinkedList04.java
+++ Data_Structures/JAVA/src/01SinglyLinkedList/SLinkedList04.java
@@ -1,54 +1,63 @@
 public class SLinkedList04 {
-    class Node{
+
+    public class Node{
         int data;
         Node next;
 
         Node(int data){
             this.data = data;
         }
-        }
-
+    }
     public Node head = null;
     public Node tail = null;
-    
-    public void addNode(int data){
-      Node newObj = new Node(data);
-         if(head == null){
+
+    public void insert(int data){
+        Node newObj = new Node(data);
+        if(head == null){
             head = newObj;
-         }else{
+        }else{
             tail.next = newObj;
-         }
-         tail = newObj;
+        }
+        tail = newObj;//itration
     }
 
     public void display(){
         Node temp = head;
-       while(temp != null){
-           System.out.println(temp.data);
-           temp = temp.next;
-       }
-    }
-
-    public void reverse(){
-        Node temp = head,prev = null,next = null;
-        while(temp != null){
-          prev = temp;
-          next = temp.next;
-          if(prev.next == next && next.next == null){
-            head = next;
-            head.next = prev;
-            
-          }
+        while(temp!=null){
+            System.out.println(temp.data);
+            temp = temp.next;
         }
     }
-   public static void main(String[] args) {
-     SLinkedList04 list = new SLinkedList04();
-    list.addNode(4);
-    list.addNode(5);
-    list.addNode(6);
-    // list.reverse();
-    list.display();
 
-   }
-   
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+    
+        while (current != null) {
+            next = current.next; // Save the next node
+    
+            current.next = prev; // Reverse the link
+    
+            prev = current;
+            current = next;
+        }
+    
+        // Update the head and tail pointers
+        tail = head;
+        head = prev;
+    }
+    
+    public static void main(String[] args) {
+        SLinkedList04 list = new SLinkedList04();
+        list.insert(5);
+        list.insert(6);
+        list.insert(7);
+        list.reverse();
+
+        list.display();
+    }
+
+
+    
 }
